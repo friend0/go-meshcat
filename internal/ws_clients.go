2930f06779b1423b9736e6aad7073d96475ef030
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2024-06-24 01:13:31 -0700

    robustify websockets implementation
    allows connections with multiple clients

---
+++ internal/ws_clients.go
@@ -0,0 +1,119 @@
+package internal
+
+import (
+	"bytes"
+	"log"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo/v4"
+)
+
+const (
+  pingPeriod  = (pongWait * 9) / 10
+	maxMessageSize = 8192
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+type Client struct {
+	hub *Hub
+
+	conn *websocket.Conn
+
+	send chan []byte
+}
+
+// readPump pumps messages from the websocket connection to the hub.
+//
+// The application runs readPump in a per-connection goroutine. The application
+// ensures that there is at most one reader on a connection by executing all
+// reads from this goroutine.
+func (c *Client) readPump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		c.hub.broadcast <- message
+	}
+}
+
+// writePump pumps messages from the hub to the websocket connection.
+//
+// A goroutine running writePump is started for each connection. The
+// application ensures that there is at most one writer to a connection by
+// executing all writes from this goroutine.
+func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				// The hub closed the channel.
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.BinaryMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			// Add queued chat messages to the current websocket message.
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				w.Write(newline)
+				w.Write(<-c.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
+
+// serveWs handles websocket requests from the peer.
+func (s *Server) serveWs() echo.HandlerFunc {
+  return func(c echo.Context) error {
+    conn, err := upgrader.Upgrade(c.Response().Writer, c.Request(), nil)
+    if err != nil {
+      log.Println(err)
+      return err
+    }
+    client := &Client{hub: s.Hub, conn: conn, send: make(chan []byte, 256)}
+    client.hub.register <- client
+
+    // Allow collection of memory referenced by the caller by doing all work in
+    // new goroutines.
+    go client.writePump()
+    go client.readPump()
+    return nil
+  }
+}
--- internal/ws_service.go
+++ internal/ws_service.go
@@ -2,21 +2,72 @@ package internal
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"sync"
 	"time"
 
 	"github.com/gorilla/websocket"
-	"github.com/labstack/echo/v4"
 )
 
 var writeWait = 10 * time.Second
-var pongWait = 60 * time.Second
 
-// var pingPeriod = (pongWait * 9) / 10
+const pongWait = 60 * time.Second
 
-// todo: implement hub and read/write pump as in chat example so that we can properly manage multiple connections
+type Hub struct {
+	// Registered clients
+	clients map[*Client]bool
+
+	// Messages to broadcast to clients.
+	broadcast chan []byte
+
+	// Register requests from the clients.
+	register chan *Client
+
+	// Unregister requests from clients.
+	unregister chan *Client
+}
+
+func NewHub() *Hub {
+  hub := &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+  go hub.run()
+  return hub
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+      fmt.Println("got a message for broadcast")
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
+
+func (h *Hub) Write(message []byte) error {
+  fmt.Println("Got here...")
+  h.broadcast <- message
+  fmt.Println("got here too")
+  return nil
+}
 
 // Define the WebSocket upgrader
 var upgrader = websocket.Upgrader{
@@ -29,44 +80,44 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func wsprocess(ws *websocket.Conn) {
-	// defer ws.Close()
-	for {
-		// Read message from WebSocket
-		messageType, message, err := ws.ReadMessage()
-		if err != nil {
-			log.Println("Read:", err, messageType)
-			break
-		}
-		log.Printf("Received: %s", message)
+// func wsprocess(ws *websocket.Conn) {
+// 	// defer ws.Close()
+// 	for {
+// 		// Read message from WebSocket
+// 		messageType, message, err := ws.ReadMessage()
+// 		if err != nil {
+// 			log.Println("Read:", err, messageType)
+// 			break
+// 		}
+// 		log.Printf("Received: %s", message)
 
-		// Echo the message back to the client
-		err = ws.WriteMessage(messageType, message)
-		if err != nil {
-			log.Println("Write:", err)
-			break
-		}
-	}
-}
+// 		// Echo the message back to the client
+// 		err = ws.WriteMessage(messageType, message)
+// 		if err != nil {
+// 			log.Println("Write:", err)
+// 			break
+// 		}
+// 	}
+// }
 
-func (s *Server) WsHandler() echo.HandlerFunc {
-	// Define a handler for WebSocket connections
-	return func(c echo.Context) error {
-		// Upgrade the HTTP connection to a WebSocket connection
-		ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-		if err != nil {
-			return err
-		}
-		ws.SetWriteDeadline(time.Now().Add(pongWait))
-		ws.SetPongHandler(func(string) error { ws.SetWriteDeadline(time.Now().Add(pongWait)); fmt.Println("pongers"); return nil })
-		ws.SetPingHandler(func(appData string) error {
-			ws.SetWriteDeadline(time.Now().Add(writeWait))
-			ws.WriteMessage(websocket.PingMessage, nil)
-			return nil
-		})
-		ws.SetReadLimit(75 * 1024 * 1024)
-		s.WS = ws
-		go wsprocess(s.WS)
-		return nil
-	}
-}
+// func (s *Server) WsHandler() echo.HandlerFunc {
+// 	// Define a handler for WebSocket connections
+// 	return func(c echo.Context) error {
+// 		// Upgrade the HTTP connection to a WebSocket connection
+// 		ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		ws.SetWriteDeadline(time.Now().Add(pongWait))
+// 		ws.SetPongHandler(func(string) error { ws.SetWriteDeadline(time.Now().Add(pongWait)); fmt.Println("pongers"); return nil })
+// 		ws.SetPingHandler(func(appData string) error {
+// 			ws.SetWriteDeadline(time.Now().Add(writeWait))
+// 			ws.WriteMessage(websocket.PingMessage, nil)
+// 			return nil
+// 		})
+// 		ws.SetReadLimit(75 * 1024 * 1024)
+// 		s.WS = ws
+// 		go wsprocess(s.WS)
+// 		return nil
+// 	}
+// }
